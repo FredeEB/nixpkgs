37bf1d8c964f7c2d39c26aad7bbcb417b7b1ce67
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-11 17:37:18 +0100

    squash! libgweather: 3.28.2 -> 3.31.91
    libgweather: 3.28.2 -> 3.32.0

--- pkgs/development/libraries/libgweather/default.nix
+++ pkgs/development/libraries/libgweather/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libgweather";
-  version = "3.31.91";
+  version = "3.32.0";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "09ndzwhn8dsxkr4df73bm62609wpg1jpylg6191dp03shk5qakv0";
+    sha256 = "04qxm5jgj5fzjb06ghwqx3c2qsc502arrfw0xl9f09wb58wjp6ny";
   };
 
   nativeBuildInputs = [ meson ninja pkgconfig gettext vala gtk-doc docbook_xsl docbook_xml_dtd_43 gobject-introspection python3 ];
