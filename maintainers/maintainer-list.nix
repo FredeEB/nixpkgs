a9f0ec82c01f80917a3ac887a368aa118cf15ee6
Frede Braendstrup <frederikbraendstrup@gmail.com>
2025-02-17 15:14:31 +0100

    maintainers: update fredeb's info

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7993,7 +7993,7 @@
     githubId = 53809656;
   };
   fredeb = {
-    email = "im@fredeb.dev";
+    email = "frederikbraendstrup@gmail.com";
     github = "FredeEB";
     githubId = 7551358;
     name = "Frede Emil";
